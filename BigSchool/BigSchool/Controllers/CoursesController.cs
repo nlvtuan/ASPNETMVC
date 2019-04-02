c9e8554a7df29a9f472e8a03abe86e774a6a6921
lvtuan <lvtuan@fit.hcmus.edu.vn>
2019-04-02 21:11:09 +0700

    Server side validation

---
+++ BigSchool/BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,54 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]        
+        public ActionResult Create()
+        {     
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel view)
+        {
+            if (!ModelState.IsValid)
+            {
+                view.Categories = _dbContext.Categories.ToList();
+                return View("Create", view);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = view.GetDateTime(),
+                CategoryId = view.Category,
+                Place = view.Place
+            };
+
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
