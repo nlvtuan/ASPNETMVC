604597fc73af4a3f2fcae2d6a33683163aa33f8b
lvtuan <lvtuan@fit.hcmus.edu.vn>
2019-04-10 07:49:44 +0700

    Add attendance controller

--- BigSchool/BigSchool/Controllers/HomeController.cs
+++ BigSchool/BigSchool/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using BigSchool.Models;
+using BigSchool.ViewModels;
 
 namespace BigSchool.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upCommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+
+            return View(upCommingCourses);
         }
 
         public ActionResult About()
